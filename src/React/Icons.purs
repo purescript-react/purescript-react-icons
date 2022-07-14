8b72395ebd9ba4e37da756a8acbc4df3b6416e4d
andys8 <andys8@users.noreply.github.com>
2022-07-14 13:42:08 +0200

    Bugfix for icon_ helper

--- src/React/Icons.purs
+++ src/React/Icons.purs
@@ -2,11 +2,12 @@ module React.Icons (icon, icon_) where
 
 import Prim.Row (class Union)
 import React.Basic (JSX, element)
+import React.Icons.Fa (faCheck)
 import React.Icons.Types (ReactIcon, PropsIcon)
 import Unsafe.Coerce (unsafeCoerce)
 
 icon :: forall a b. Union a b PropsIcon => ReactIcon -> Record a -> JSX
 icon reactIcon props = element (unsafeCoerce reactIcon) props
 
-icon_ :: forall a b. Union a b PropsIcon => ReactIcon -> JSX
+icon_ :: ReactIcon -> JSX
 icon_ reactIcon = icon reactIcon {}
